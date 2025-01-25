7a52d32a729561145d3c9b28040bea09c2dc3890
Saurav Kumar <144691979+sauravk58@users.noreply.github.com>
2025-01-25 16:27:16 +0530

    Add files via upload

---
+++ frontend/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import ScraperForm from "../components/ScraperForm";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ScraperForm />
+    </div>
+  );
+}
